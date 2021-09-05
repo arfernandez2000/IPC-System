a38408ef70aebe4c2f1d2dd6211e97455072756b
arfernandez2000 <arfernandez@itba.edu.ar>
2021-09-05 12:24:13 -0300

    starting

--- src/master.c
+++ src/master.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define ERROR 1
+
 
 int main(int argc, char const *argv[]){
-
+    if(argc < 2) {
+        fprintf(stderr, "Incorrect number of arguments: %s/n", argv[0]);
+        exit(ERROR);
+    }
     
     setvbuf(stdout,NULL,_IONBF,0); 
    
